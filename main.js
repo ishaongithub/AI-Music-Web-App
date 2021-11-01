6862f4c9e31165dabeb022aae3da5a2abce74200
ishaongithub <75757332+ishaongithub@users.noreply.github.com>
2021-11-01 12:17:14 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 song="";
+leftWristX=0;
+leftWristY=0;
+rightWristX=0;
+rightWristY=0;
 function preload(){
     song=loadSound("music.mp3");
+    //load all sounds
 }
 
 function setup(){
@@ -9,13 +14,35 @@ function setup(){
 
     video=createCapture(VIDEO);
     video.hide();
+    poseNet=ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
 }   
 function draw(){
     image(video,0,0,600,500);
 }
 function play(){
-    song.play();
-}
-function stop(){
-    song.stop();
-}
+   song.play();
+   song.setVolume(1);
+   song.rate(1);
+       
+   }
+   
+   function stop()
+   {
+        song.stop();
+   }
+   function modelLoaded(){
+     console.log('PoseNet Is Initialized');
+   }
+   function gotPoses(results){
+     if(results.length>0){
+       console.log(results);
+       leftWristX=results[0].pose.leftWrist.x;
+       leftWristY=results[0].pose.leftWrist.y;
+       console.log("left wrist x= "+leftWristX+ " left wrist y= "+leftWristY);
+
+       rightWristX=results[0].pose.rightWrist.x;
+       rightWristY=results[0].pose.rightWrist.y;
+       console.log("right wrist x= "+rightWristX+ " right wrist y= "+rightWristY);
+     }
+   }
